e80bf93b880279b6d81b3378c3652066878198f6
linxiuke <linxiuke@outlook.com>
2018-10-11 15:48:16 +0800

    mmp swagger页面出来了但是接口文档出不来 给所有接口都加上api前缀 技术不够操作来凑

--- src/main/java/com/zero/common/base/config/SecurityConfig.java
+++ src/main/java/com/zero/common/base/config/SecurityConfig.java
@@ -34,18 +34,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring()
-                .antMatchers(HttpMethod.OPTIONS, "/**")
-                .antMatchers("/app/**/*.{js,html}")
-                .antMatchers("/bower_components/**")
-                .antMatchers("/i18n/**")
-                .antMatchers("/swagger-ui/index.html")
-                .antMatchers("/h2-console/**")
-        ;
-
-    }
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring()
+//                .antMatchers(HttpMethod.OPTIONS, "/**")
+//                .antMatchers("/app/**/*.{js,html}")
+//                .antMatchers("/bower_components/**")
+//                .antMatchers("/i18n/**")
+//                .antMatchers("/swagger-ui/index.html")
+//                .antMatchers("/h2-console/**")
+//        ;
+//
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -61,15 +61,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 // swagger start
-                .antMatchers("/swagger-ui.html").permitAll()
-                .antMatchers("/swagger-resources/configuration/ui").permitAll()
-                .antMatchers("/images/**").permitAll()
-                .antMatchers("/webjars/**").permitAll()
-                .antMatchers("/v2/api-docs").permitAll()
-                .antMatchers("/configuration/ui").permitAll()
-                .antMatchers("/configuration/security").permitAll()
+//                .antMatchers("/swagger-ui.html").permitAll()
+//                .antMatchers("/swagger-resources/configuration/ui").permitAll()
+//                .antMatchers("/images/**").permitAll()
+//                .antMatchers("/webjars/**").permitAll()
+//                .antMatchers("/v2/api-docs").permitAll()
+//                .antMatchers("/configuration/ui").permitAll()
+//                .antMatchers("/configuration/security").permitAll()
                 // swagger end
                 //  yml配置的context-path: /base  不能加上
+                // mmp swagger页面出来了但是接口文档出不来 给所有接口都加上api前缀 技术不够操作来凑
                 .antMatchers("/api/user/signIn").permitAll()
                 .antMatchers("/api/**").authenticated()
                 .and()
--- src/main/java/com/zero/common/core/CommonRequestHeader.java
+++ src/main/java/com/zero/common/core/CommonRequestHeader.java
@@ -1,5 +1,6 @@
 package com.zero.common.core;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -9,7 +10,15 @@ import lombok.Data;
 @Data
 public class CommonRequestHeader {
 
+    /**
+     * version 版本号
+     */
+    @ApiModelProperty(value = "应用版本号", dataType = "string", example = "1.0.0")
     private String version;
 
+    /**
+     * 平台类型
+     */
+    @ApiModelProperty(value = "平台类型", dataType = "string", example = "ios")
     private String platform;
 }
